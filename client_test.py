3bb6c27d588ea4a0261f0130eb7f1346a347a53b
Anonsa <anonsa.panos@gmail.com>
2024-01-14 02:06:12 -0500

    added client test

--- client_test.py
+++ client_test.py
@@ -7,7 +7,12 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
+  
     """ ------------ Add the assertion below ------------ """
+    # took the raw data and ran it through the function to verify results
+    result = getDataPoint(quotes[0])
+    expected_result = ("ABC", 120.48, 121.2, (120.48 + 121.2)/2)
+    self.assertEqual(result,expected_result)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
